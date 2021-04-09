da615447d99b81c6a5cb0b3e50826c8eaaef6be7
Fredrik <fredrikf2000@gmail.com>
2021-04-09 15:24:24 +0200

    styled login button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Hotels from "./pages/Hotels";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/hotels" component={Hotels} />
           <Route path="/contact" component={Contact} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="nav">
-      <div className="nav__logo">Holidaze</div>
+      <Link to="/" className="nav__logo">
+        Holidaze
+      </Link>
 
       <ul className="nav__links">
         <Link to="/" className="nav__link">
@@ -15,6 +17,9 @@ const Navbar = () => {
         </Link>
         <Link to="/contact" className="nav__link">
           Contact
+        </Link>
+        <Link to="/login" className="nav__login">
+          Login
         </Link>
       </ul>
     </nav>
