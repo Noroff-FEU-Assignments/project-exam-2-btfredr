00ae40683a08ea5bcfb2a571938d09d211cab220
Fredrik <fredrikf2000@gmail.com>
2021-04-11 13:19:18 +0200

    made landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import Navigation from "../components/Navigation";
 import background from "../assets/background.jpg";
+import Paragraph from "../components/Paragraph";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,9 +9,22 @@ const Home = () => {
       <Navigation />
 
       <div className="home__container">
-        <img src={background}></img>
+        <img src={background} alt="Hotel"></img>
+
         <div className="home__box">
-          <h1>Looking for a place to stay in Bergen?</h1>
+          <h1 className="home__title">
+            Looking for a place to stay in Bergen?
+          </h1>
+          <span className="home__bar"></span>
+          <div className="home__text">
+            <Paragraph content="High quality rooms starting at 399 kr" />
+          </div>
+
+          <div>
+            <Link to="/hotels" className="home__btn">
+              View Hotels
+            </Link>
+          </div>
         </div>
       </div>
     </div>
