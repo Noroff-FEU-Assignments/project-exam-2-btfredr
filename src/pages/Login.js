b0dfa8d7b21d948629487c634c134ba3e1eba14a
Fredrik <fredrikf2000@gmail.com>
2021-04-12 16:08:40 +0200

    started working on login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,17 @@
 import Navigation from "../components/Navigation";
+import { loginSchema } from "../utils/schemas";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { BASE_URL, AUTH_PATH } from "../utils/constants";
+import axios from "axios";
+import AuthContext from "../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <Navigation />
-      <h1>Login</h1>
+      Login
     </div>
   );
 };
