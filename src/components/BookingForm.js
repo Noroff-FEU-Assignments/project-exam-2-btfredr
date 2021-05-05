c56f16304582eb3aa22413e40aded8e08b521338
Fredrik <fredrikf2000@gmail.com>
2021-05-05 13:13:15 +0200

    moved booking form into its own component

---
+++ src/components/BookingForm.js
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import { enquirySchema } from "../utils/schemas";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+
+const BookingForm = ({ hotel }) => {
+  const [submitting, setSubmitting] = useState(false);
+  const [postError, setPostError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const enquiryPath = BASE_URL + "/enquiries";
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(enquirySchema),
+  });
+
+  const onSubmit = async (data) => {
+    setSubmitting(true);
+    setPostError(null);
+    console.log(data);
+    try {
+      const response = await axios.post(enquiryPath, data);
+      console.log(response);
+      setSuccess(true);
+    } catch (error) {
+      console.log("error", error);
+      setPostError(error.toString());
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <form className="bookingForm" onSubmit={handleSubmit(onSubmit)}>
+      {postError && <p>{postError}</p>}
+      <fieldset disabled={submitting}>
+        <label>Name</label>
+        <input
+          type="string"
+          name="Name"
+          ref={register}
+          placeholder="Enter your full name..."
+        />
+        {errors.Name && (
+          <span className="form__error">{errors.Name.message}</span>
+        )}
+
+        <label>Email</label>
+        <input
+          type="string"
+          name="email"
+          ref={register}
+          placeholder="Enter your email..."
+        />
+        {errors.email && (
+          <span className="form__error">{errors.email.message}</span>
+        )}
+        <div className="hotel__dates">
+          <div className="hotel__dateContainer">
+            <label>Check in</label>
+            <input type="date" name="startDate" ref={register} />
+            {errors.startDate && (
+              <span className="form__error">{errors.startDate.message}</span>
+            )}
+          </div>
+          <div className="hotel__dateContainer">
+            <label>Check out</label>
+            <input type="date" name="endDate" ref={register} />
+            {errors.endDate && (
+              <span className="form__error">{errors.endDate.message}</span>
+            )}
+          </div>
+        </div>
+        <label>Guests</label>
+        <select name="capacity" ref={register}>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        {errors.capacity && (
+          <span className="form__error">{errors.capacity.message}</span>
+        )}
+        {success ? (
+          <p className="form__success">Your booking was confirmed!</p>
+        ) : null}
+        <button type="submit" className="form__btn">
+          {submitting ? "Booking ..." : "Book"}
+        </button>
+        <label>Price</label>
+        <p>{hotel.price} NOK</p>
+        <label>Cleaning and service fees:</label>
+        <p>249 NOK</p>
+        <label>Total</label>
+        <p>Total: </p>
+      </fieldset>
+    </form>
+  );
+};
+
+export default BookingForm;
--- src/pages/Hotel.js
+++ src/pages/Hotel.js
@@ -1,47 +1,17 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 import Heading from "../components/Heading";
 import Paragraph from "../components/Paragraph";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import axios from "axios";
-import { enquirySchema } from "../utils/schemas";
-
+import BookingForm from "../components/BookingForm";
 import Spinner from "../assets/Spinner.gif";
+import { useParams } from "react-router-dom";
 
 const Hotel = () => {
   const [hotel, setHotel] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const [submitting, setSubmitting] = useState(false);
-  const [postError, setPostError] = useState(null);
-  const [success, setSuccess] = useState(null);
-
-  const enquiryPath = BASE_URL + "/enquiries";
-
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(enquirySchema),
-  });
-
-  const onSubmit = async (data) => {
-    setSubmitting(true);
-    setPostError(null);
-    console.log(data);
-    try {
-      const response = await axios.post(enquiryPath, data);
-      console.log(response);
-      setSuccess(true);
-    } catch (error) {
-      console.log("error", error);
-      setPostError(error.toString());
-    } finally {
-      setSubmitting(false);
-    }
-  };
 
   const { id } = useParams();
 
@@ -95,71 +65,7 @@ const Hotel = () => {
             <Paragraph content={hotel.description} />
           </div>
           <div className="hotel__right">
-            <form className="bookingForm" onSubmit={handleSubmit(onSubmit)}>
-              {postError && <p>{postError}</p>}
-              <label>Name</label>
-              <input
-                type="string"
-                name="Name"
-                ref={register}
-                placeholder="Enter your full name..."
-              />
-              {errors.Name && (
-                <span className="form__error">{errors.Name.message}</span>
-              )}
-
-              <label>Email</label>
-              <input
-                type="string"
-                name="email"
-                ref={register}
-                placeholder="Enter your email..."
-              />
-              {errors.email && (
-                <span className="form__error">{errors.email.message}</span>
-              )}
-              <div className="hotel__dates">
-                <div className="hotel__dateContainer">
-                  <label>Check in</label>
-                  <input type="date" name="startDate" ref={register} />
-                  {errors.startDate && (
-                    <span className="form__error">
-                      {errors.startDate.message}
-                    </span>
-                  )}
-                </div>
-                <div className="hotel__dateContainer">
-                  <label>Check out</label>
-                  <input type="date" name="endDate" ref={register} />
-                  {errors.endDate && (
-                    <span className="form__error">
-                      {errors.endDate.message}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <label>Guests</label>
-              <select name="capacity" ref={register}>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
-              {errors.capacity && (
-                <span className="form__error">{errors.capacity.message}</span>
-              )}
-              {success ? (
-                <p className="form__success">Your booking was confirmed!</p>
-              ) : null}
-              <button type="submit" className="form__btn">
-                {submitting ? "Booking ..." : "Book"}
-              </button>
-              <label>Price</label>
-              <p>{hotel.price} NOK</p>
-              <label>Cleaning and service fees:</label>
-              <p>249 NOK</p>
-              <label>Total</label>
-              <p>Total: </p>
-            </form>
+            <BookingForm hotel={hotel} />
           </div>
         </div>
       </div>
